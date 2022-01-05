6cfe0ad74c9f8c3cef839df515ed9a433546bdc4
Celian HAMON <skelletondude@gmail.com>
2022-01-05 08:43:20 +0100

    updated spam with the option to select the number of time it spam

--- bot/spam.go
+++ bot/spam.go
@@ -1,6 +1,7 @@
 package bot
 
 import (
+	"strconv"
 	"strings"
 
 	"github.com/bwmarrin/discordgo"
@@ -8,8 +9,14 @@ import (
 
 func spam(arg []string, channelID string, m *discordgo.MessageCreate, s *discordgo.Session) string {
 	send := s.ChannelMessageSend
+
+	nombreSpam, stringErr := strconv.Atoi(arg[1])
+	if stringErr != nil {
+		_, _ = send(channelID, m.Author.Username+", veuillez renseigner le nombre de spam a éffectuer comme ceci : "+arg[0]+" <nombre de spam> <texte a spam>")
+	}
+	arg[1] = ""
 	if len(arg) > 1 {
-		for i := 0; i < 4; i++ {
+		for i := 0; i < nombreSpam; i++ {
 			_, err := send(channelID, strings.Join(arg[1:], " "))
 			if err != nil {
 				return err.Error()
