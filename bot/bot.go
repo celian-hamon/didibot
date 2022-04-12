f5b03f07b45fd7b508e5c569d6bf6c57670f76ac
p2acoo <theotruvelott@gmail.com>
2022-04-12 16:53:10 +0200

    add first version for getting timetable of school with api. Need some bugfix...

--- bot/bot.go
+++ bot/bot.go
@@ -35,7 +35,7 @@ func Start() {
 		return
 	}
 	fmt.Println("Bot is running")
-	
+
 }
 
 func messageHandler(s *discordgo.Session, m *discordgo.MessageCreate) {
@@ -46,15 +46,15 @@ func messageHandler(s *discordgo.Session, m *discordgo.MessageCreate) {
 		cmd := arg[0]
 		if cmd == "time" {
 			err, reply := timer(arg, m.ChannelID, m, s)
-			security.Log(cmd, arg, err,reply, m)
+			security.Log(cmd, arg, err, reply, m)
 		}
 		if cmd == "sondage" {
 			err, reply := sondage(arg, m.ChannelID, m, s)
-			security.Log(cmd, arg, err,reply, m)
+			security.Log(cmd, arg, err, reply, m)
 		}
 		if cmd == "echo" {
 			err, reply := echo(arg, m.ChannelID, m.Message.ID, m, s)
-			security.Log(cmd, arg, err,reply, m)
+			security.Log(cmd, arg, err, reply, m)
 		}
 		if cmd == "spam" {
 			if !security.IsAdmin(m.Author.ID, config.AdminList) {
@@ -64,20 +64,24 @@ func messageHandler(s *discordgo.Session, m *discordgo.MessageCreate) {
 				}
 			} else {
 				err, reply := spam(arg, m.ChannelID, m, s)
-				security.Log(cmd, arg[1:], err,reply, m)
+				security.Log(cmd, arg[1:], err, reply, m)
 			}
 
 		}
 		if cmd == "help" {
 			err, reply := help(arg, m.ChannelID, m.Message.Author.ID, m, s)
-			security.Log(cmd, arg, err,reply, m)
+			security.Log(cmd, arg, err, reply, m)
 		}
 		if cmd == "drop" {
 			err, reply := pokemon.Drop(arg, m, s)
 			security.Log(cmd, arg, err, reply, m)
 		}
-		if cmd == "ratio"{
+		if cmd == "ratio" {
 			err, reply := ratio(arg, m.ChannelID, m.Message.ID, "", m, s)
+			security.Log(cmd, arg, err, reply, m)
+		}
+		if cmd == "edt" {
+			err, reply := edt(arg, m.ChannelID, m.Message.ID, m, s)
 			security.Log(cmd, arg, err, reply, m)
 		}
 	}
--- go.mod
+++ go.mod
@@ -1,14 +1,15 @@
 module discordbot
 
-go 1.17
+go 1.18
 
 require (
-	github.com/bwmarrin/discordgo v0.23.2
+	github.com/bwmarrin/discordgo v0.24.0
 	github.com/mtslzr/pokeapi-go v1.4.0
 )
 
 require (
-	github.com/gorilla/websocket v1.4.0 // indirect
+	github.com/gorilla/websocket v1.4.2 // indirect
 	github.com/patrickmn/go-cache v2.1.0+incompatible // indirect
-	golang.org/x/crypto v0.0.0-20181030102418-4d3f4d9ffa16 // indirect
+	golang.org/x/crypto v0.0.0-20210421170649-83a5a9bb288b // indirect
+	golang.org/x/sys v0.0.0-20201119102817-f84b799fce68 // indirect
 )
