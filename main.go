9e565f15b14d3e936c5c4e53c41b1ef70bd1b18f
celian-hamon <skelletondude@gmail.com>
2022-05-28 23:46:01 +0200

    bon

--- main.go
+++ main.go
@@ -8,17 +8,22 @@ import (
 	"discordbot/bot"
 )
 
-func main() {
+func handler(w http.ResponseWriter, r *http.Request) {
 	err := config.ReadConfig()
 	if err != nil {
 		return
 	}
 	go bot.Start()
+}
+
+func main() {
 
 	port := os.Getenv("PORT")
 	if port == "" {
 		port = "8080"
 	}
+
+	http.HandleFunc("/", handler)
 	http.ListenAndServe(":"+port, nil)
 
 	<-make(chan struct{})
